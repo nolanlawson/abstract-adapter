4e4a438fbb5742da9594768fd421991c16683f01
Nolan Lawson <nolan.lawson@gmail.com>
2014-05-23 19:57:17 -0700

    wip

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 'use strict';
 
 var PouchDB = require('pouchdb');
-var LevelPouch = PouchDB.adapters.leveldb;
+var LevelPouch = require('./node_modules/pouchdb/lib/adapters/leveldb.js');
 var utils = PouchDB.utils;
 
-function AbstractAdapter(opts, callback) {
-  var _opts = utils.extend({
-    db: leveldown
-  }, opts);
+function AbstractAdapterFactory(leveldown) {
 
-  LevelPouch.call(this, _opts, callback);
+  function AbstractAdapter(opts, callback) {
+    var _opts = utils.extend({
+      db: leveldown
+    }, opts);
+
+    LevelPouch.call(this, _opts, callback);
+  }
+
+  // overrides for normal LevelDB behavior on Node
+  AbstractAdapter.valid = function () {
+    return leveldown.valid();
+  };
+  AbstractAdapter.use_prefix = leveldown.usePrefix;
+
+  AbstractAdapter.destroy = utils.toPromise(function (name, opts, callback) {
+    if (typeof opts === 'function') {
+      callback = opts;
+      opts = {};
+    }
+    var _opts = utils.extend({
+      db: leveldown
+    }, opts);
+
+    return LevelPouch.destroy(name, _opts, callback);
+  });
+  AbstractAdapter.adapterName = leveldown.adapterName;
+  AbstractAdapter.needsMigration = false;
+  return AbstractAdapter;
 }
 
-// overrides for normal LevelDB behavior on Node
-AbstractAdapter.valid = function () {
-  return leveldown.valid();
-};
-AbstractAdapter.use_prefix = leveldown.usePrefix;
-
-AbstractAdapter.destroy = utils.toPromise(function (name, opts, callback) {
-  if (typeof opts === 'function') {
-    callback = opts;
-    opts = {};
-  }
-  var _opts = utils.extend({
-    db: leveldown
-  }, opts);
-
-  return LevelPouch.destroy(name, _opts, callback);
-});
-AbstractAdapter.adapterName = leveldown.adapterName;
-
-module.exports = AbstractAdapter;
+module.exports = AbstractAdapterFactory;
